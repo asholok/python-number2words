3824e870a27cfea341b79399f44cba7bbbdd07a3
asholok <asholok@gmail.com>
2014-10-30 20:52:10 +0200

    add english and russin languages

---
+++ languages/eng.py
@@ -0,0 +1,26 @@
+preffix = 'eng'
+
+teens = {1: 'one ', 2: 'two ', 3 : 'three ', 4: 'four ', 5: 'five ', 6: 'six ', 7: 'seven ', 8: 'eigth ', 
+ 9: "nine ", 10: 'ten ', 11: 'eleven ', 12: 'twelve ', 13: 'thirteen ', 14: 'fourteen ',
+ 15: "fifteen ", 16: 'sixteen ', 17: 'seventeen ', 18: 'eigthteen ', 19: "nineteen "}
+dozens = {2: 'twenty ', 3: 'thirty ', 4: 'fourty ', 5: "fifty ", 6: 'sixty ', 7: 'seventy ',
+ 8: 'eigthty ', 9: "ninety "}
+hundreds = {1: 'one hundred ', 2: 'two hundreds ', 3: 'three hundreds ', 4: 'four hundreds ', 5: 'five hundreds ', 6: 'six hundreds ', 7: 'seven hundreds ',
+ 8: 'eigth hundreds ', 9: "nine hundreds "}
+cpecial_case = {1: 'one ', 2: 'two '}
+currency_uah = 'hrivna'
+currency_rub = 'rubl'
+currency_eur = 'euro'
+currency_usd = 'dollar'
+coins = 'kopek'
+coins_cent = 'cent'
+level = {1: 'thausand', 2: 'millon', 3: 'billion', 4: 'trillion', 5: 'quadrillion'}
+currency_endings_uah = ['', 's', 's']
+currency_endings_rub = ['', 's', 's']
+endings_eur = ['', 's', 's']
+thousand_endings = [' ', 's ', 's ']
+upper_mln_endings = [' ', 's ', 's ']
+currency_endings_usd = ['', 's', 's']
+coins_endings = ['', 's', 's']
+
+CURRENCIES = ['eur', 'rub', 'uah', 'usd']
--- languages/languages.py
+++ languages/languages.py
@@ -1,41 +1,15 @@
-#!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
-
-teens_ua = {1: 'один ', 2: 'двa ', 3 : 'три ', 4: 'чотири ', 5: 'п\'ять ', 6: 'шість ', 7: 'сім ', 8: 'вісім ', 
- 9: "дев'ять ", 10: 'десять ', 11: 'одинадцять ', 12: 'дванадцять ', 13: 'тринадцять ', 14: 'чотирнадцять ',
- 15: "п'ятнадцять ", 16: 'шістнадцять ', 17: 'сімнадцять ', 18: 'вісімнадцять ', 19: "дев'ятнадцять "}
-dozens_ua = {2: 'двадцять ', 3: 'тридцять ', 4: 'сорок ', 5: "п'ятдесят ", 6: 'шістдесят ', 7: 'сімдесят ',
- 8: 'вісімдесят ', 9: "дев'яносто "}
-hundreds_ua = {1: 'сто ', 2: 'двісті ', 3: 'триста ', 4: 'чотириста ', 5: "п'ятсот ", 6: 'шістсот ', 7: 'сімсот ',
- 8: 'вісімсот ', 9: "дев'ятсот "}
-cpecial_case_ua = {1: 'одна ', 2: 'дві '}
-currency_uah = 'грив'
-currency_rub = 'руб'
-currency_eur = 'євро'
-currency_usd = 'доллар'
-coins_ua = 'копій'
-coins_cent = 'цент'
-level_ua = {1: 'тисяч', 2: 'мільйон', 3: 'мільярд', 4: 'трильйон', 5: 'квадрильйон'}
-currency_endings_uah = ['ня', 'ні', 'ень'] 
-currency_endings_rub = ['ель', 'лі', 'лів']
-currency_endings_eur = ['', '', '']
-thousand_endings_ua = ['a ', 'і ', ' ']
-upper_mln_endings_ua = [' ', 'и ', 'ів ']
-currency_endings_usd = ['', 'и', 'ів']
-coins_endings_ua = ['ка ', 'ки ', 'ок ']
-
-CURRENCIES = ['eur', 'rub', 'uah', 'usd']
-
-class Ua(object):
-	def __init__(self, currency_value):
-		self.teens = teens_ua
-		self.dozens = dozens_ua
-		self.hundreds = hundreds_ua
-		self.cpecial_case = cpecial_case_ua
-		self.level = level_ua
-		self.thousand_endings = thousand_endings_ua
-		self.upper_mln_endings = upper_mln_endings_ua
-		
+class Blanck(object):
+	def __init__(self, root):
+		self.root = root
+		self.teens = root.teens
+		self.dozens = root.dozens
+		self.hundreds = root.hundreds
+		self.cpecial_case = root.cpecial_case
+		self.level = root.level
+		self.thousand_endings = root.thousand_endings
+		self.upper_mln_endings = root.upper_mln_endings
+	
+	def __call__(self, currency_value):
 		if currency_value == 'uah':
 			self._set_uah()
 		if currency_value == 'rub':
@@ -46,25 +20,25 @@ class Ua(object):
 			self._set_usd()
 	
 	def _set_uah(self):
-		self.currency = currency_uah
-		self.currency_endings = currency_endings_uah
-		self.coins = coins_ua
-		self.coins_endings = coins_endings_ua
+		self.currency = self.root.currency_uah
+		self.currency_endings = self.root.currency_endings_uah
+		self.coins = self.root.coins
+		self.coins_endings = self.root.coins_endings
 
 	def _set_rub(self):
-		self.currency = currency_rub
-		self.currency_endings = currency_endings_rub
-		self.coins = coins_ua
-		self.coins_endings = coins_endings_ua
+		self.currency = self.root.currency_rub
+		self.currency_endings = self.root.currency_endings_rub
+		self.coins = self.root.coins
+		self.coins_endings = self.root.coins_endings
 
 	def _set_usd(self):
-		self.currency = currency_usd
-		self.currency_endings = currency_endings_usd
-		self.coins = coins_cent
-		self.coins_endings = upper_mln_endings_ua
+		self.currency = self.root.currency_usd
+		self.currency_endings = self.root.currency_endings_usd
+		self.coins = self.root.coins_cent
+		self.coins_endings = self.root.upper_mln_endings
 
 	def _set_eur(self):
-		self.currency = currency_eur
-		self.currency_endings = currency_endings_eur
-		self.coins = coins_cent
-		self.coins_endings = upper_mln_endings_ua
+		self.currency = self.root.currency_eur
+		self.currency_endings = self.root.currency_endings_eur
+		self.coins = self.root.coins_cent
+		self.coins_endings = self.root.upper_mln_endings
